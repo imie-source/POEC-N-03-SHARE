398d87f0248c86127f05db563d714dab6099dd2a
simon <simon.louvet@imie.fr>
2015-04-01 12:25:13 +0200

    correction TPA Injection

---
+++ Injection Dependance/FilmLocationWeb/src/fr/imie/web/FilmServlet.java
@@ -0,0 +1,50 @@
+package fr.imie.web;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.imie.Iservice.FilmServiceInterface;
+
+/**
+ * Servlet implementation class FilmServlet
+ */
+@WebServlet("/films")
+public class FilmServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Inject private FilmServiceInterface filmService;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FilmServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		request.setAttribute("films", filmService.getFilms());
+		
+		request.getRequestDispatcher("/WEB-INF/films.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
