8fc3cbe95cb5d46c766769205f2fb9fbc1d3dbd8
simon <simon.louvet@imie.fr>
2015-04-01 14:13:47 +0200

    correction TP2 Injection

--- Injection Dependance/FilmLocationWeb/src/fr/imie/web/FilmServlet.java
+++ Injection Dependance/FilmLocationWeb/src/fr/imie/web/FilmServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import fr.imie.Iservice.FilmServiceInterface;
+import fr.imie.service.qualifier.French;
 
 /**
  * Servlet implementation class FilmServlet
